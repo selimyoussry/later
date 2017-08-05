d2996a2af780178ecb0abe25f46a52b05cee63b0
selim <selim@wearkinetic.com>
2017-08-05 11:13:55 -0400

    changing the database interface model to make heavier use of instance IDs

--- database.go
+++ database.go
@@ -8,8 +8,8 @@ import (
 
 // Database lists what the database needs to be able to do for this library
 type Database interface {
-	AbortInstances(name string, parameters []byte) ([]string, error)
-	CreateInstance(name string, executionTime time.Time, parameters []byte) (string, error)
+	AbortInstances(taskName string, parameters []byte) ([]string, error)
+	CreateInstance(taskName string, executionTime time.Time, parameters []byte) (string, error)
 	GetInstances(start, end time.Time) ([]*structures.Instance, error)
 	GetLastPullTime() (*time.Time, error)
 	SetPullTime(t time.Time) error
